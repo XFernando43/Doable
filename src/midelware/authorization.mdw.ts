4297fd717f7a05a522953eadd1c0c1537049bfec
XFernando43 <U20171E997@upc.edu.pe>
2024-02-05 17:26:31 -0500

    add dependecies

---
+++ src/midelware/authorization.mdw.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+// import { userService } from "../Services/users.service";
+import { ApiError } from "./error";
+import { User } from "../Domain/models/user.entity";
+
+// import { userService } from "../Services/users.service";
+  
+export function authorize(...allowedRoles: User["role"][]) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    
+    const role = req.userRole;
+    if (!role) return next(new ApiError("No autorizado", 401));
+ 
+    if (allowedRoles.includes(role as User["role"])) {
+      next();
+    } else {
+      next(new ApiError("Acceso denegado", 403));
+    }
+    
+      
+  };
+}
+ 
