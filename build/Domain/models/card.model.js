50c9fd8cc440e86e44633d309050e970aa305c8e
XFernando43 <U20171E997@upc.edu.pe>
2024-02-09 00:46:15 -0500

    ahhhhh por que no se deployea si funciona

--- build/Domain/models/card.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Card = void 0;
-class Card {
-    card_id;
-    card_title;
-    list_Id;
-}
-exports.Card = Card;
--- build/db/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.truncateTable = void 0;
-const _1 = require(".");
-const truncateTable = async (tableName) => {
-    await (0, _1.query)(`TRUNCATE TABLE ${tableName} RESTART IDENTITY CASCADE`);
-};
-exports.truncateTable = truncateTable;
