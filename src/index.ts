c9dd7cb617e204ba7e72e2bc329cb206673bf841
XFernando43 <U20171E997@upc.edu.pe>
2024-02-09 01:47:37 -0500

    add cors permisions

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,16 @@ import { listRouter } from './App/routers/list.router';
 import { cardRouter } from './App/routers/card.router';
 import "dotenv/config";
 
-
 export const app = express();
 const port = process.env["PORT"]
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 
 app.use(express.json());
 app.use("/users",userRouter);
