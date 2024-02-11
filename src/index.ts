6cd2c8e7e4a92a240612f0e2ebc2155c1d19caf2
XFernando43 <U20171E997@upc.edu.pe>
2024-02-10 19:51:01 -0500

    cors policy

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const port = process.env["PORT"]
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
